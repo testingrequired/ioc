556c8c71b637862f6d701f1bb3583011ad26a4ab
Kylee Tilley <kyleetilley@gmail.com>
2019-01-21 20:35:40 -0600

    Adding babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import babel from "rollup-plugin-babel";
+
 export default {
   input: "src/index.js",
   output: {
     file: "dist/ject.js",
     format: "cjs"
-  }
+  },
+  plugins: [
+    babel({
+      exclude: "node_modules/**"
+    })
+  ]
 };
