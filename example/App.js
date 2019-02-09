f6cb25d14274e44054f936989d89107214db4205
Kylee Tilley <kyleetilley@gmail.com>
2019-02-09 13:48:23 -0600

    Initial implementation for lifetime

--- example/App.js
+++ example/App.js
@@ -1,5 +1,6 @@
 import { inject, component } from "../src/index";
 import RandomNumbers from "./RandomNumbers";
+import RandomNumber from "./RandomNumber";
 import Message from "./Message";
 
 @component
@@ -7,8 +8,16 @@ export default class App {
   @inject(Message) message;
   @inject(RandomNumbers) random;
   @inject(RandomNumbers) random2;
+  @inject(RandomNumber) randomNumber;
+  @inject(RandomNumber) randomNumber2;
 
   greet() {
-    console.log(this.message, this.random, this.random2);
+    console.log(
+      this.message,
+      this.random,
+      this.random2,
+      this.randomNumber,
+      this.randomNumber2
+    );
   }
 }
--- example/RandomNumbers.js
+++ example/RandomNumbers.js
@@ -1,9 +1,8 @@
-import { register } from "../src/index";
+import { component } from "../src/index";
 
+@component.instance
 export default class RandomNumbers {
   get value() {
     return Math.floor(Math.random() * (10000 - 0) + 0);
   }
 }
-
-register(RandomNumbers);
